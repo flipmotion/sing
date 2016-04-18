f9db7846f77d86c038431b158482de84ff088c1b
igor <flipmotions@gmail.com>
2016-04-18 19:02:48 +0300

    Up main page category
    Up main page category

--- build/js/my/app.js
+++ build/js/my/app.js
@@ -1,2 +1,44 @@
 //use strict mode;
 'use strict';
+//owl import
+import owlCarousel from '../owl.carousel.min.js';
+
+
+$(document).ready(function(){
+	let owl = $('[data-item="slider"]');
+	let owlPreview = $('[data-item="slider-preview"]');
+
+	owl.owlCarousel({
+		loop:true,
+		margin:0,
+		nav:true,
+		dots:true,
+		items:1,
+		autoplayHoverPause: true,
+		autoplayTimeout: 5000,
+		autoplay:true,
+		navText: [
+		"<i class='my-arrow-left'></i>", 
+		"<i class='my-arrow-right'></i>"
+		],
+		dots: true
+	});
+
+	owlPreview.owlCarousel({
+		items:1,
+		thumbs: true,
+		thumbsPrerendered: true
+	});
+
+	var headerH = $('.header').height();
+	var sliderH = $('.Slider-main').height();
+
+	$("#my-nav").affix({
+		offset: { 
+			top: headerH + sliderH 
+		}
+	});
+	$("#my-nav").on('affixed.bs.affix', function(){
+		console.log("Меню навигации была прикреплена. Теперь она не прокручивается вместе со страницей.");
+	});
+});
