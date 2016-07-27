98622c72f82893b4a1a664b8fb1e9f4504d05192
igor <flipmotions@gmail.com>
2016-07-27 12:35:51 +0300

    Checkout
    Checkout

--- build/js/my/app.js
+++ build/js/my/app.js
@@ -564,6 +564,82 @@ $(document).ready(function(){
 	if(numberInput) {
 		numberInput.on('keypress', NumberIn);
 	}
+
+	$('.btn-number').click(function(e){
+		e.preventDefault();
+
+		var fieldName = $(this).attr('data-field');
+		var type      = $(this).attr('data-type');
+		var input = $("input[name='"+fieldName+"']");
+		var currentVal = parseInt(input.val());
+		if (!isNaN(currentVal)) {
+			if(type == 'minus') {
+
+				if(currentVal > input.attr('min')) {
+					input.val(currentVal - 1).change();
+				} 
+				if(parseInt(input.val()) == input.attr('min')) {
+					$(this).attr('disabled', true);
+				}
+
+			} else if(type == 'plus') {
+
+				if(currentVal < input.attr('max')) {
+					input.val(currentVal + 1).change();
+				}
+				if(parseInt(input.val()) == input.attr('max')) {
+					$(this).attr('disabled', true);
+				}
+
+			}
+		} else {
+			input.val(0);
+		}
+	});
+	$('.input-number').focusin(function(){
+		$(this).data('oldValue', $(this).val());
+	});
+	$('.input-number').change(function() {
+
+		var minValue =  parseInt($(this).attr('min'));
+		var maxValue =  parseInt($(this).attr('max'));
+		var valueCurrent = parseInt($(this).val());
+
+		var name = $(this).attr('name');
+		if(valueCurrent >= minValue) {
+			console.log(name);
+			$(".btn-number[data-type='minus'][data-field='"+name+"']").removeAttr('disabled');
+
+		} else {
+			alert('Sorry, the minimum value was reached');
+			$(this).val($(this).data('oldValue'));
+		}
+		if(valueCurrent <= maxValue) {
+			$(".btn-number[data-type='plus'][data-field='"+name+"']").removeAttr('disabled')
+		} else {
+			
+			$(this).val($(this).data('oldValue'));
+		}
+
+
+	});
+	$(".input-number").keydown(function (e) {
+
+		if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 190]) !== -1 ||
+
+			(e.keyCode == 65 && e.ctrlKey === true) || 
+
+			(e.keyCode >= 35 && e.keyCode <= 39)) {
+
+			return;
+	}
+
+	if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
+		e.preventDefault();
+	}
+
+	
+});
 });
 /*START LKFORM*/
 var LKFORM = {};
